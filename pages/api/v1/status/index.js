cc54b3f50eb97be441818052a7185757cf93f5d6
william ribeiro <0171708@feevale.br>
2025-01-22 02:35:56 +0000

    finish `/api/v1/status` endpoint.

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "/infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Hello, World!" });
+  const updatedAt = new Date().toISOString();
+  const dbVersionQuery = await database.query("SHOW server_version;");
+  const dbVersionResult = dbVersionQuery.rows[0].server_version; //query resuls are stored on rows[n] attribute
+
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsResult = dbMaxConnectionsQuery.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbCurrentConnectionsQuery = await database.query({
+    text: "SELECT COUNT(*)::int AS used_connections FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+
+  const dbCurrentConnectionsResult =
+    dbCurrentConnectionsQuery.rows[0].used_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionResult,
+        max_connections: parseInt(dbMaxConnectionsResult),
+        current_connections: parseInt(dbCurrentConnectionsResult),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log("updated_at: " + responseBody.updated_at);
+  console.log("db_version: " + responseBody.dependencies.database.version);
+  console.log(
+    "max_connections: " + responseBody.dependencies.database.max_connections,
+  );
+  console.log(
+    "current_connections: " +
+      responseBody.dependencies.database.current_connections,
+  );
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.current_connections).toBe(1);
+
+  // expect(responseBody.dependencies.database_version).toBeDefined();
+  // expect(responseBody.dependencies.max_connections).toBeDefined();
+  // expect(responseBody.dependencies.current_connections).toBeDefined();
+  // expect(responseBody.dependencies.max_connections).toBeGreaterThan(0);
+  // expect(
+  //   responseBody.database.dependencies.current_connections,
+  // ).toBeGreaterThan(0);
 });
